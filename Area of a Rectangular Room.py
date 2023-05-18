42845b6d07cfa385c37780e408a5bdd5b6afcebc
DzilafhoPia <dzilafhopia17@gmail.com>
2023-05-18 17:28:50 +0200

    Initial commit

---
+++ Area of a Rectangular Room.py
@@ -0,0 +1,12 @@
+# Create a program that calculates the area of a room, display the area in both square fit and square meters.
+
+print("What is the length of the room? ")
+length = input()
+print("What is the width of the room? ")
+width = input()
+print("You entered dimensions of " + length + " feet by " + width + " feet.")
+print("The area is ")
+area = int(length) * int(width)
+print(str(area) + " square feet")
+square_meters = area * round(float(0.09290304), 6)
+print(str(square_meters) + " square meters")
